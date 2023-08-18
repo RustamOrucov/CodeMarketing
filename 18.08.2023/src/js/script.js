69c0b175d7795b3a9bdd281b229625b76ec4ca76
orucov <orucov.r@inbox.ru>
2023-08-18 17:15:15 +0400

    commit

---
+++ 18.08.2023/src/js/script.js
@@ -0,0 +1,34 @@
+const pupil = document.querySelectorAll(".pupil");
+const pupilCenter = document.querySelectorAll(".pupil-center");
+const eyeLid=document.querySelectorAll('.eye-lid')
+
+
+
+setInterval(function(){
+    eyeLid.forEach(el=>{
+      el.style.height=`${500}px`
+    })
+},4000)
+setInterval(function(){
+    eyeLid.forEach(el=>{
+      el.style.height=`${70}px`
+    })
+},4020)
+
+
+
+
+window.addEventListener("mousemove", (e) => {
+  let x = e.clientX;
+  let y = e.clientY;
+  pupil.forEach((el) => {
+    
+       el.style.marginLeft=(x/10-15+'px')
+       el.style.marginTop=(y/10-5)+'px'
+     
+  });
+  pupilCenter.forEach((el) => {
+    el.style.marginLeft=x/10-65+'px'
+       el.style.marginTop=y/10-30+'px'
+  });
+});
