06e722046c2195cf5b57584addd86fb5845b3a99
orucov <orucov.r@inbox.ru>
2023-09-01 10:43:37 +0400

    commit

---
+++ Responsive MObile First Cards CM/src/js/script.js
@@ -0,0 +1,40 @@
+const rightBtn = document.querySelector(".fa-angle-right");
+const leftBtn = document.querySelector(".fa-angle-left");
+const cardWrap = document.querySelector(".card-wrapper");
+const card = cardWrap.querySelectorAll(".card");
+const container = document.querySelector(".cards").offsetWidth;
+
+let translateX = 0;
+
+// kenarda qalan kart sayi
+let l = card.length;
+let z = Math.round(container / card[0].offsetWidth);
+let d = l - z;
+
+// right btn 
+rightBtn.addEventListener("click", () => {
+   if (d > 0) {
+    console.log(d);
+    translateX += card[0].offsetWidth;
+    leftBtn.style.display = "block";
+    cardWrap.style.transform = `translateX(-${translateX}px)`;
+    d--;
+  }
+    else {
+      rightBtn.style.display = "none";
+    
+  }
+});
+
+//left btn
+leftBtn.addEventListener("click", () => {
+  if (translateX >= card[0].offsetWidth) {
+    translateX -= card[0].offsetWidth;
+    cardWrap.style.transform = `translateX(-${translateX}px)`;
+    rightBtn.style.display = "block";
+    d++
+  } else {
+    leftBtn.style.display = "none";
+  }
+});
+
